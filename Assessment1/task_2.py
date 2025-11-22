f648bb1a9d242040f279330e9595b0efad2b1761
Vijay Sunar <vijaysunar1214@gmail.com>
2025-11-22 23:34:27 +0530

    Added Assessment1 folder with Task 1 and Task 2

---
+++ Assessment1/task_2.py
@@ -0,0 +1,7 @@
+first_name = input("enter the first name: ")
+last_name = input("enter the last name: ")
+
+full_name = first_name+" "+last_name
+print(f"hello,{full_name} welcome to python program")
+
+
