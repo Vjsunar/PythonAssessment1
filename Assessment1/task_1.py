c38dc26820f87b994be619396333f587351e263e
Vijay Sunar <vijaysunar1214@gmail.com>
2025-11-22 23:31:16 +0530

    Clean commit with only Assessment1 and README

--- Assessment1/task_1.py
+++
@@ -1,7 +0,0 @@
-num1=int(input("enter the first number: "))
-num2=int(input("enter the second number: "))
-
-print(f" sum of two number is :{num1+num2}")
-print(f" Subtraction  of two number is :{num1-num2}")
-print(f" Multiplication  of two number is :{num1*num2}")
-print(f" Division  of two number is :{num1/num2}")
--- Assessment1/task_2.py
+++
@@ -1,7 +0,0 @@
-first_name = input("enter the first name: ")
-last_name = input("enter the last name: ")
-
-full_name = first_name+" "+last_name
-print(f"hello,{full_name} welcome to python program")
-
-
